efd275e2c125bf0a6c7f05da0e68a2c34e5a0dd7
jagadeesh9395 <jagadeeshwarrao.k@gmail.com>
2025-11-09 21:39:24 +0530

    Added index page

--- rpf/src/main/java/com/kjr/rpf/controller/HomeController.java
+++ rpf/src/main/java/com/kjr/rpf/controller/HomeController.java
@@ -2,12 +2,15 @@ package com.kjr.rpf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
-        return "redirect:/resumes/upload";
+        return "index";
     }
+    
 }
